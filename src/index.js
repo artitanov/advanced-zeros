ad0235c7e3086220d11f9acb95ce45880ebdb57d
artyom Titanov <artitanov@gmail.com>
2019-03-02 18:46:25 +0300

    Ready Task, 100 of 100

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+    let arrOfBase = [];
+    let finalDivide = [0];
+    let countOfFinalDivide = [0];
+    let variableOfNulls = [];
+    k = 1;
+    let cons = 0;
+    for(let i = 2; i <= base; i++){
+      
+      while(base % i == 0 && base > 1){
+        arrOfBase.push(i);
+        Divider = i;
+        if (finalDivide[k-1]< Divider) {++k; finalDivide.push(Divider); countOfFinalDivide[k-1] = 1; cons = 0;};
+        if (finalDivide[k-1]= Divider) {cons++; countOfFinalDivide[k-1] = cons;};
+        base = base / i;
+      }
+        if(base/i == 1){
+          arrOfBase.push(base);
+        }
+      }
+    let x = number;
+    sumOfNulls = 0;
+      for (let l = 1; finalDivide.length > l; l++){
+        while (x >= finalDivide[l]) {
+          x = Math.floor(x / finalDivide[l]); 
+          sumOfNulls = sumOfNulls + x;};
+          variableOfNulls[l-1] = sumOfNulls;
+          variableOfNulls[l-1] = Math.floor(variableOfNulls[l-1] / countOfFinalDivide[l]);
+          x = number;
+          sumOfNulls = 0;
+        };
+    return Math.min(...variableOfNulls);
 }
